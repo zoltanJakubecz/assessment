b2de119bc3a5e8977b70867d6bc76166862f8560
Zoltan Jakubecz <jakuzo@gmail.com>
2021-12-10 12:31:27 +0100

    add nubers to hundred

--- js-numerals/src/service/convert.ts
+++ js-numerals/src/service/convert.ts
@@ -1,17 +1,51 @@
-export const convertToPhrase = (number: number) => {
-    let res = [];
+import {numPhrases, wholes} from './phrases';
+
+export const convertToPhrase = (number: any ) => {
+    let resultArray = [];
     let minus = false;
 
     if(number == 0){
         return "zero";
     }
 
-    console.log("HELLY");
 
     // Validation check. The number should be between -999999 and 999999 and not a letter.
     if(number > 999999 || number < -999999) return "Invalid number. Please try again";
 
-    const isNumber = (n: any) => {
+    const isNumber = (n: any): boolean => {
         return !isNaN(parseFloat(n)) && !isNaN(n - 0)
     }
+
+
+    if(!isNumber(number)) return "Invalid input. Please try again";
+
+    if(number < 0){
+        number = -number;
+        minus = true;
+    }
+
+    let toHundred = zeroToHundredFunc(number % 100);
+
+    if(toHundred) resultArray.unshift(toHundred);
+
+}
+
+
+const zeroToHundredFunc = (number: number) => {
+
+    let result =
+        // one digit
+            number / 10 < 1 ? numPhrases[number][0] :
+        // between 10 and 20 spacial named numbers
+            number % 10 > 0 && numPhrases[number % 10][1] && number < 20 ? numPhrases[number % 10][1] :
+
+            number > 13 && number < 20 ? numPhrases[number % 10][0] + "teen" :
+        // above 19
+            numPhrases[Math.floor(number / 10)][2] ? numPhrases[Math.floor(number / 10)][2] : numPhrases[Math.floor(number / 10)][0] + "ty";
+
+    if(number % 10 > 0 && number > 20){
+        result += "-" + numPhrases[number % 10][0];
+    }
+
+    return result;
 }
---
+++ js-numerals/src/service/phrases.ts
@@ -0,0 +1,13 @@
+export const numPhrases: {[key:number]: string[]} = {
+    1 : ["one","eleven", "ten"],
+    2 : ["two","twelve", "twenty"],
+    3 : ["three","thirteen", "thirty"],
+    4 : ["four"],
+    5 : ["five","fifteen", "fifty"],
+    6 : ["six"],
+    7 : ["seven"],
+    8 : ["eight","eighteen", "eighty"],
+    9 : ["nine"]
+}
+
+export const wholes = [" hundred", " thousand", " million"]
