059f4bd28c7d18ce0eb0d9ea7d2e15e31adce264
Zoltan Jakubecz <jakuzo@gmail.com>
2021-12-12 11:18:38 +0100

    add getOneTodo method

--- node-todo/src/controller/todo-controller.ts
+++ node-todo/src/controller/todo-controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import data from '../data/data.json';
 
 
-export function getTasks(req: Request, res: Response) {
+export const getTasks = (req: Request, res: Response): void => {
     new Promise((resolve, reject) => {
 
        resolve(data.todos);
@@ -13,5 +13,17 @@ export function getTasks(req: Request, res: Response) {
     })
     .catch((reason) => {
         res.send(reason)
+    })
+}
+
+export const getOneTask = (req: Request, res: Response): void => {
+    new Promise((resolve, rejects) => {
+        const id = req.params.id;
+        resolve(data.todos.find(todo => todo.id == id));
+    }).then(todo => {
+        if(!todo) res.send("Todo not exists");
+        res.send(todo);
+    }).catch(reason => {
+        res.send(reason);
     })
 }
--- node-todo/src/routes/todosRoute.ts
+++ node-todo/src/routes/todosRoute.ts
@@ -1,6 +1,7 @@
 import * as core from 'express-serve-static-core';
-import { getTasks } from '../controller/todo-controller';
+import { getOneTask, getTasks } from '../controller/todo-controller';
 
 export function routes(app: core.Express){
     app.get('/todos', getTasks);
+    app.get('/todos/:id', getOneTask);
 }
